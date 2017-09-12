a4316da800059a0ac915325d7cd858f3d291de78
chenchao <cc@stlc.cn>
2017-09-12 16:49:14 +0800

    1

---
+++ src/main/java/com/cc/mblog/login/MbLogLoginApplication.java
@@ -0,0 +1,12 @@
+package com.cc.mblog.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbLogLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbLogLoginApplication.class, args);
+	}
+}
