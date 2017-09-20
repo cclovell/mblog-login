ed331664263f02c1f556285be439a784a347976c
chenchao <cc@stlc.cn>
2017-09-20 15:56:12 +0800

    1

--- src/main/java/com/cc/mblog/MbLogLoginApplication.java
+++ src/main/java/com/cc/mblog/MbLogLoginApplication.java
@@ -1,11 +1,11 @@
-package com.cc.mblog.login;
+package com.cc.mblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class MbLogLoginApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MbLogLoginApplication.class, args);
 	}
---
+++ src/main/java/com/cc/mblog/service/LoginServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cc.mblog.service;
+
+/**
+ * Created by chenchao on 2017/9/18.
+ */
+public class LoginServiceImpl implements LoginService {
+    @Override
+    public void test() {
+        System.out.println("test");
+    }
+}
